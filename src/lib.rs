522b0f17bb127c6a41d4fb08ab4b7f53019aa9cb
UserIsntAvailable <pinedap.angel@gmail.com>
2023-04-12 07:40:39 -0400

    feat: finish api.

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,51 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+#![warn(rust_2018_idioms, clippy::pedantic)]
+
+const fn fourcc_impl<const IS_LITTLE: bool>(four: &str) -> u32 {
+    assert!(four.len() == 4, "FourCC must be 4 ASCII characters");
+
+    let mut result = 0;
+    let mut remaining = four.as_bytes();
+
+    while let [head @ .., current] = remaining {
+        assert!(current.is_ascii(), "FourCC must only have ASCII characters");
+
+        let current = *current as u32;
+        result = if IS_LITTLE {
+            (result >> 8) | (current << 24)
+        } else {
+            (result << 8) | current
+        };
+
+        remaining = head;
+    }
+
+    result
+}
+
+#[inline]
+pub const fn fourcc(four: &str) -> u32 {
+    fourcc_impl::<{ cfg!(target_endian = "little") }>(four)
+}
+
+#[inline]
+pub const fn fourcc_le(four: &str) -> u32 {
+    fourcc_impl::<true>(four)
+}
+
+#[inline]
+pub const fn fourcc_be(four: &str) -> u32 {
+    fourcc_impl::<false>(four)
+}
+
+// TODO: Maybe `fourcc_from_bytes`?
+
+#[inline]
+pub fn is_fourcc<Bytes>(four: Bytes) -> bool
+where
+    Bytes: AsRef<[u8]>,
+{
+    let bytes = four.as_ref();
+    bytes.len() == 4 && bytes.iter().all(|b| b.is_ascii())
 }
 
 #[cfg(test)]
@@ -7,8 +53,29 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn le_works() {
+        const _: () = assert!(fourcc_le("avc1") == 0x61766331);
+    }
+
+    #[test]
+    fn be_works() {
+        const _: () = assert!(fourcc_be("avc1") == 0x31637661);
+    }
+
+    #[test]
+    fn is_works() {
+        assert!(is_fourcc("avc1"))
+    }
+
+    #[test]
+    #[should_panic(expected = "FourCC must be 4 ASCII characters")]
+    fn str_is_not_4_len() {
+        fourcc("avc");
+    }
+
+    #[test]
+    #[should_panic(expected = "FourCC must only have ASCII characters")]
+    fn str_constains_non_ascii_chars() {
+        fourcc("神 ");
     }
 }
